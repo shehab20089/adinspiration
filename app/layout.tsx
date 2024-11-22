05e7300a6771411b9fff659fe6aa5974d0c10099
shehab20089 <shehabmohsen1995@gmail.com>
2024-11-22 23:57:43 +0200

    feat: add the basic skeleton of the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/shared/Navbar";
+import { Sidebar } from "@/components/shared/Sidebar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Adinspiration",
@@ -25,10 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${inter.className} antialiased`}>
+        <Navbar />
+        <div className="">
+          <Sidebar />
+          {children}
+        </div>
       </body>
     </html>
   );
---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <header>
+      <h1>Adinspiration</h1>
+      <nav>
+        <Link href="/">Home</Link>
+      </nav>
+    </header>
+  );
+};
